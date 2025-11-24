e3f7834981549e66ac214dc58562744ebed31ea7
CaioSNery <caionery40@gmail.com>
2025-11-24 10:57:42 -0300

    chat

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.Ollama;
 
 var builder = Kernel.CreateBuilder()
 .AddOllamaChatCompletion(modelId:"llama3.1:latest", 
@@ -14,3 +15,30 @@ builder.Services
 var kernel = builder.Build();
 
 var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+OllamaPromptExecutionSettings settings = new()
+{
+    FunctionChoiceBehavior=FunctionChoiceBehavior.Auto()
+};
+
+var history = new ChatHistory();
+string ? prompt;
+
+do
+{
+    Console.Write("User >");
+    prompt = Console.ReadLine();
+
+    history.AddUserMessage(prompt);
+
+    var result = await chatCompletionService
+    .GetChatMessageContentAsync(
+        history,
+        executionSettings: settings,
+        kernel:kernel);
+
+        Console.WriteLine("Assistant > " + result);
+
+        history.AddMessage(result.Role, result.Content ?? string.Empty);
+    
+}while(prompt is not null);
