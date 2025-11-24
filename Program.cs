56d5305d84d318b35b2c818ed7bb7bce0a871be6
CaioSNery <caionery40@gmail.com>
2025-11-24 10:45:33 -0300

    create :  logging SemantiKernel

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+var builder = Kernel.CreateBuilder()
+.AddOllamaChatCompletion(modelId:"llama3.1:latest", 
+endpoint:new Uri("http://localhost:11434"));
+
+builder.Services
+.AddLogging(x=>x.AddConsole()
+.SetMinimumLevel(LogLevel.Trace));
+
+var kernel = builder.Build();
+
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
